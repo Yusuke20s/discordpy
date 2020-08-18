3e2809907b6b5964a0bdb34a97982cd268075ef1
Yusuke20s <yuusuke0486@gmail.com>
2020-08-19 00:51:43 +0900

    おみくじ

--- おみくじ.py
+++ おみくじ.py
@@ -21,15 +21,15 @@ async def on_message(message):
         return
     
     if message.content == "おみくじ":
-        embed2 = discord.Embed(title="おみくじ", description=f"{message.author.mention}さんの今日の運勢",color=0x03fc90)
-        embed2.set_thumbnail(url=message.author.avatar_url)
-        embed2.add_field(name="[運勢]", value=random.choice(("大吉", "中吉", "小吉", "吉", "凶", "大凶")), inline=False)
-        embed2.add_field(name="[総合運]", value=random.choice(("★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆")), inline=False)
-        embed2.add_field(name="[恋愛運]", value=random.choice(("★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆")), inline=False)
-        embed2.add_field(name="[金運]", value=random.choice(("★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆")), inline=False)
-        embed2.add_field(name="[健康運]", value=random.choice(("★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆")), inline=False)
+        embed = discord.Embed(title="おみくじ", description=f"{message.author.mention}さんの今日の運勢",color=0x03fc90)
+        embed.set_thumbnail(url=message.author.avatar_url)
+        embed.add_field(name="[運勢]", value=random.choice(("大吉", "中吉", "小吉", "吉", "凶", "大凶")), inline=False)
+        embed.add_field(name="[総合運]", value=random.choice(("★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆")), inline=False)
+        embed.add_field(name="[恋愛運]", value=random.choice(("★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆")), inline=False)
+        embed.add_field(name="[金運]", value=random.choice(("★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆")), inline=False)
+        embed.add_field(name="[健康運]", value=random.choice(("★★★★★", "★★★★☆", "★★★☆☆", "★★☆☆☆", "★☆☆☆☆")), inline=False)
         #メッセージは適当
-        embed2.add_field(name="[一言アドバイス]", value=random.choice(("一生懸命頑張ると吉", "無理をしない程度に頑張ると吉", "早く寝ると吉", "一日楽しむと吉", "趣味に没頭すると吉")), inline=False)
-        await message.channel.send(embed=embed2)
+        embed.add_field(name="[一言アドバイス]", value=random.choice(("一生懸命頑張ると吉", "無理をしない程度に頑張ると吉", "早く寝ると吉", "一日楽しむと吉", "趣味に没頭すると吉")), inline=False)
+        await message.channel.send(embed=embed)
 
 client.run(TOKEN)
