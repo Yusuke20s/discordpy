0634d8013bd44bc5467cf361a362332d9b9ad05e
Yusuke20s <yuusuke0486@gmail.com>
2021-05-17 21:57:44 +0900

    [Add] メッセージ送信

---
+++ メッセージ送信.py
@@ -0,0 +1,29 @@
+import discord
+import asyncio
+
+# token.txtファイルからTOKENの読み込み
+with open("token.txt") as f:
+    TOKEN = f.read()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("logged in\n")
+    asyncio.ensure_future(greeting_gm())
+
+async def greeting_gm():
+    while True:
+        content = input("送信メッセージを入力:")
+        
+        CHANNEL_ID = input("送信チャンネルIDを入力:")
+
+        try:
+            channel = client.get_channel(int(CHANNEL_ID))
+            await channel.send(content)
+        except:
+            pass
+
+        await asyncio.sleep(1)
+
+client.run(TOKEN)
