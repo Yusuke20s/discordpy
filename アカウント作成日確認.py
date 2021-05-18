47b2450463eb2f91ec6bbea9b963fcdcd87d4e5c
Yusuke20s <yuusuke0486@gmail.com>
2021-05-18 23:18:21 +0900

    [Update] アカウント作成日確認

--- アカウント作成日確認.py
+++ アカウント作成日確認.py
@@ -20,7 +20,6 @@ async def on_message(message):
     # Discordアカウント作成日(世界標準時)
     if message.content == "いつ":
         day = message.author.created_at.strftime("%Y/%m/%d")
-        print(f"{message.author.name}のアカウント作成日は{day}")
         embed = discord.Embed(title="あなたのアカウント作成日", description=f"{message.author.mention}さん",color=0x2ECC69)
         embed.add_field(name="世界標準時(UTC)",value=day)
         await message.channel.send(embed=embed)
