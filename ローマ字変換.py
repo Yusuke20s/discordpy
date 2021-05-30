17f73cd3e7b26b1c8241b5588040bce7f97c9a6a
Yusuke20s <yuusuke0486@gmail.com>
2021-05-30 13:14:12 +0900

    [Update] ローマ字変換

--- ローマ字変換.py
+++ ローマ字変換.py
@@ -20,14 +20,21 @@ h1d5={"mma":"っま","mmi":"っみ","mmu":"っむ","mme":"っめ","mmo":"っも"
 h1d6={"rra":"っら","rri":"っり","rru":"っる","rre":"っれ","rro":"っろ"}
 h1d6={"yya":"っや","yyi":"っい","yyu":"っゆ","yye":"っいぇ","yyo":"っよ"}
 h1d7={"wwa":"っわ","wwi":"っうぃ","wwu":"っう","wwe":"っうぇ","wwo":"っを"}
+h1d8={"gga":"っが","ggi":"っぎ","ggu":"っぐ","gge":"っげ","ggo":"っご"}
+h1d9={"zza":"っざ","zzi":"っじ","zzu":"っず","zze":"っぜ","zzo":"っぞ"}
+h1d10={"jja":"っざ","jji":"っじ","jju":"っず","jje":"っぜ","jjo":"っぞ"}
+h1d11={"dda":"っだ","ddi":"っぢ","ddu":"っづ","dde":"っで","ddo":"っど"}
+h1d12={"bba":"っば","bbi":"っび","bbu":"っぶ","bbe":"っべ","bbo":"っぼ"}
+h1d13={"ppa":"っぱ","ppi":"っぴ","ppu":"っぷ","ppe":"っぺ","ppo":"っぽ"}
 
-h1d8={"xtu":"っ","xya":"ゃ","xyu":"ゅ","xyo":"ょ"}
-h1d9={"ltu":"っ","lya":"ゃ","lyu":"ゅ","lyo":"ょ"}
-h1d10={"xa":"ぁ","xi":"ぃ","xu":"ぅ","xe":"ぇ","xo":"ぉ"}
-h1d11={"la":"ぁ","li":"ぃ","lu":"ぅ","le":"ぇ","lo":"ぉ"}
-h1d12={"shi":"し","chi":"ち","tsu":"つ"}
+h1d14={"xtu":"っ","xya":"ゃ","xyu":"ゅ","xyo":"ょ"}
+h1d15={"ltu":"っ","lya":"ゃ","lyu":"ゅ","lyo":"ょ"}
+h1d16={"xa":"ぁ","xi":"ぃ","xu":"ぅ","xe":"ぇ","xo":"ぉ"}
+h1d17={"la":"ぁ","li":"ぃ","lu":"ぅ","le":"ぇ","lo":"ぉ"}
+h1d18={"shi":"し","chi":"ち","tsu":"つ"}
 
-hiragana_dict1.update(**h1d2, **h1d3, **h1d4, **h1d5, **h1d6, **h1d7, **h1d8, **h1d9, **h1d10, **h1d11, **h1d12)
+hiragana_dict1.update(**h1d2, **h1d3, **h1d4, **h1d5, **h1d6, **h1d7, **h1d8, **h1d9, **h1d10)
+hiragana_dict1.update(**h1d11, **h1d12, **h1d13, **h1d14, **h1d15, **h1d16, **h1d17, **h1d18)
 
 # 基本変換
 hiragana_dict2={"ka":"か","ki":"き","ku":"く","ke":"け","ko":"こ"}
@@ -54,17 +61,18 @@ h2d19={"mya":"みゃ","myi":"みぃ","myu":"みゅ","mye":"みぇ","myo":"みょ
 h2d20={"rya":"りゃ","ryi":"りぃ","ryu":"りゅ","rye":"りぇ","ryo":"りょ"}
 h2d21={"gya":"ぎゃ","gyi":"ぎぃ","gyu":"ぎゅ","gye":"ぎぇ","gyo":"ぎょ"}
 h2d22={"zya":"じゃ","zyi":"じぃ","zyu":"じゅ","zye":"じぇ","zyo":"じょ"}
-h2d23={"dya":"ぢゃ","dyi":"ぢぃ","dyu":"ぢゅ","dye":"ぢぇ","dyo":"ぢょ"}
-h2d24={"bya":"びゃ","byi":"びぃ","byu":"びゅ","bye":"びぇ","byo":"びょ"}
-h2d25={"pya":"ぴゃ","pyi":"ぴぃ","pyu":"ぴゅ","pye":"ぴぇ","pyo":"ぴょ"}
+h2d23={"jya":"じゃ","jyi":"じぃ","jyu":"じゅ","jye":"じぇ","jyo":"じょ"}
+h2d24={"dya":"ぢゃ","dyi":"ぢぃ","dyu":"ぢゅ","dye":"ぢぇ","dyo":"ぢょ"}
+h2d25={"bya":"びゃ","byi":"びぃ","byu":"びゅ","bye":"びぇ","byo":"びょ"}
+h2d26={"pya":"ぴゃ","pyi":"ぴぃ","pyu":"ぴゅ","pye":"ぴぇ","pyo":"ぴょ"}
 
-h2d26={"ja":"じゃ","ji":"じ","ju":"じゅ","je":"じぇ","jo":"じょ"}
-h2d27={"fa":"ふぁ","fi":"ふぃ","fu":"ふ","fe":"ふぇ","fo":"ふぉ"}
-h2d28={"va":"ヴぁ","vi":"ヴぃ","vu":"ヴ","ve":"ヴぇ","vo":"ヴぉ"}
+h2d27={"ja":"じゃ","ji":"じ","ju":"じゅ","je":"じぇ","jo":"じょ"}
+h2d28={"fa":"ふぁ","fi":"ふぃ","fu":"ふ","fe":"ふぇ","fo":"ふぉ", "fya":"ふゃ"}
+h2d29={"va":"ゔぁ","vi":"ゔぃ","vu":"ゔ","ve":"ゔぇ","vo":"ゔぉ"}
 
 hiragana_dict2.update(**h2d2, **h2d3, **h2d4, **h2d5, **h2d6, **h2d7, **h2d8, **h2d9, **h2d10)
 hiragana_dict2.update(**h2d11, **h2d12, **h2d13, **h2d14, **h2d15, **h2d16, **h2d17, **h2d18, **h2d19, **h2d20)
-hiragana_dict2.update(**h2d21, **h2d22, **h2d23, **h2d24, **h2d25, **h2d26, **h2d27, **h2d28)
+hiragana_dict2.update(**h2d21, **h2d22, **h2d23, **h2d24, **h2d25, **h2d26, **h2d27, **h2d28, **h2d29)
 
 # 一部行
 hiragana_dict3={"na":"な","ni":"に","nu":"ぬ","ne":"ね","no":"の"}
@@ -120,22 +128,22 @@ async def on_message(message):
 
         for k1 in hiragana_dict1:
             if k1 in new_content:
-                v1 = hiragana_dict1[k1]
+                v1 = hiragana_dict1[k1.lower()]
                 new_content = new_content.replace(k1, v1)
 
         for k2 in hiragana_dict2:
             if k2 in new_content:
-                v2 = hiragana_dict2[k2]
+                v2 = hiragana_dict2[k2.lower()]
                 new_content = new_content.replace(k2, v2)
 
         for k3 in hiragana_dict3:
             if k3 in new_content:
-                v3 = hiragana_dict3[k3]
+                v3 = hiragana_dict3[k3.lower()]
                 new_content = new_content.replace(k3, v3)
 
         for k4 in hiragana_dict4:
             if k4 in new_content:
-                v4 = hiragana_dict4[k4]
+                v4 = hiragana_dict4[k4.lower()]
                 new_content = new_content.replace(k4, v4)
 
         new_content = new_content.replace("'", "")
